5e236c53201aa913e6c2590f36ed6f297a4d6123
Npnk1 <nikita.pasenkov@gmail.com>
2025-10-24 15:53:14 +0200

    bruto oefening voltooid

---
+++ 05_collecties/bruto/BrutoApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Bruto
+{
+    static void Main()
+    {
+        double[] brutoSalarissen = { 2000.0, 3000.0, 4000.0 };
+        double[] nettoSalarissen = new double[3];
+        double belastingPercentage = 0.37;
+
+        nettoSalarissen[0] = brutoSalarissen[0] * (1 - belastingPercentage);
+        Console.WriteLine($"Bruto: {brutoSalarissen[0]}  →  Netto: {nettoSalarissen[0]:0.00}");
+
+        nettoSalarissen[1] = brutoSalarissen[1] * (1 - belastingPercentage);
+        Console.WriteLine($"Bruto: {brutoSalarissen[1]}  →  Netto: {nettoSalarissen[1]:0.00}");
+
+        nettoSalarissen[2] = brutoSalarissen[2] * (1 - belastingPercentage);
+        Console.WriteLine($"Bruto: {brutoSalarissen[2]}  →  Netto: {nettoSalarissen[2]:0.00}");
+    }
+}
+
