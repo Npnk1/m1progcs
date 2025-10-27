ff8263450cd7eee223e7739790fee83458f6b6be
Npnk1 <nikita.pasenkov@gmail.com>
2025-10-27 18:44:22 +0100

    varindex oefening voltooid

---
+++ 05_collecties/varindex/VarIndexApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class VarIndex
+{
+    static void Main()
+    {
+        
+        string[] namen = { "mario", "sonic", "spyro" };
+
+        
+        Console.WriteLine("Spelkarakters:");
+        for (int i = 0; i < namen.Length; i++)
+        {
+            Console.WriteLine($"{i}: {namen[i]}");
+        }
+
+        
+        Console.Write("\nKies een karakter (typ het indexnummer): ");
+        string keuze = Console.ReadLine();
+
+        
+        int gekozenIndex = Convert.ToInt32(keuze);
+
+        string gekozenNaam = namen[gekozenIndex];
+
+        Console.WriteLine($"\nJe hebt gekozen: {gekozenNaam}");
+    }
+}
